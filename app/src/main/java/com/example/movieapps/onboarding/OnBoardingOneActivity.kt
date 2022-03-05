66e6e4de43decc79b40513d6c1d7f4b926b42391
AlvinSanudharma <alvinsanudharma@gmail.com>
2022-03-05 12:04:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/movieapps/onboarding/OnBoardingOneActivity.kt
@@ -0,0 +1,12 @@
+package com.example.movieapps.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieapps.R
+
+class OnBoardingOneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_one)
+    }
+}
