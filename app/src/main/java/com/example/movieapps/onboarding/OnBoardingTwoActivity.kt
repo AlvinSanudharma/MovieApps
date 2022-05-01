6eb6f2249803a15d5bc6d3c0d11220aa9e7e481f
AlvinSanudharma <alvinsanudharma@gmail.com>
2022-05-01 11:51:37 +0800

    Setting Firebase

--- app/src/main/java/com/example/movieapps/onboarding/OnBoardingTwoActivity.kt
+++ app/src/main/java/com/example/movieapps/onboarding/OnBoardingTwoActivity.kt
@@ -1,12 +1,19 @@
 package com.example.movieapps.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.movieapps.R
+import kotlinx.android.synthetic.main.activity_on_boarding_two.*
 
 class OnBoardingTwoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding_two)
+
+        btn_home.setOnClickListener {
+            var intent = Intent(this@OnBoardingTwoActivity, OnBoardingThreeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/movieapps/sign/SignInActivity.kt
+++ app/src/main/java/com/example/movieapps/sign/SignInActivity.kt
@@ -3,10 +3,26 @@ package com.example.movieapps.sign
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.movieapps.R
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import com.google.firebase.database.DatabaseReference
+
+import com.google.firebase.database.FirebaseDatabase
+
+
+
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        button2.setOnClickListener {
+            // Write a message to the database
+            // Write a message to the database
+            val database = FirebaseDatabase.getInstance("https://movie-apps-23990-default-rtdb.firebaseio.com/")
+            val myRef = database.getReference("message")
+
+            myRef.setValue("Hello, World!")
+        }
     }
 }
